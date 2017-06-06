f39f551a597087a8bf38d00ed7dba3b5d05fe731
Kai Hoffman <hoffm154@purdue.edu>
2017-06-06 18:08:23 -0400

    Delete list item when X button pressed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const newEntry = document.querySelector('#newEntry')
+const newEntry = document.querySelector('form#newEntry')
+const listOutput = document.querySelector('div#listOutput')
+const uList = document.querySelector('ul#uList')
 
 function handleFavorite(ev) {
   ev.preventDefault()
   const btn = ev.target
-
   if (btn.value === 'false') {
     btn.style.backgroundColor = 'blue'
     btn.value = true
@@ -17,20 +18,21 @@ function handleFavorite(ev) {
 function handleDelete(ev) {
   ev.preventDefault()
   const btn = ev.target
-  console.log("delete")
+  const elem = document.getElementById(btn.id)
+  elem.remove()
 }
 
-function makeX() {
+function makeX(value) {
   const button = document.createElement('button')
   button.innerHTML = 'X'
+  button.id = value
   button.addEventListener('click', handleDelete)
   return button
 }
 
-function makeO(value) {
+function makeO() {
   const button = document.createElement('button')
   button.innerHTML = 'o'
-  button.id = value // The button id is the name of the item
   button.value = false
   button.addEventListener('click', handleFavorite)
   return button
@@ -39,37 +41,35 @@ function makeO(value) {
 function renderListItem(index, value) {
   const li = document.createElement('li')
   li.innerHTML = `${value}             `
-  const oButton = makeO(value)
-  const xButton = makeX()
+  li.id = value
+  const oButton = makeO()
+  const xButton = makeX(value)
   li.appendChild(oButton)
   li.appendChild(xButton)
   return li
 }
 
-function renderList(data) {
-  const ul = document.createElement('ul')
+function renderList(data, id) {
   Object.keys(data).map(function(index) {
     const li = renderListItem(index, data[index])
-    ul.appendChild(li)
+    if (id === 'reg') {
+      uList.appendChild(li)
+    }
+    else if (id === 'irreg') {
+      uList.prepend(li)
+    }
   })
-  return ul
+  return uList
 }
 
 function handleClick(ev) {
   ev.preventDefault()
   const ne = ev.target // should give us the object was CLICKED
-  const listOutput = document.querySelector('#listOutput')
   const itemName = newEntry.item.value
   const newThing = {
     name: itemName,
   }
-
-  if (ne.id === 'reg') {
-    listOutput.appendChild(renderList(newThing))
-  }
-  else if (ne.id === 'irreg') {
-    listOutput.prepend(renderList(newThing))
-  }
+  listOutput.appendChild(renderList(newThing, ne.id))
 }
 
 newEntry.addEventListener('click', handleClick)
